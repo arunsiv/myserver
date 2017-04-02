8bece39f0d635a76dfb3cccd4d84e6126a7f745b
Arun <Arun>
2017-04-02 14:52:16 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to access server.log file');
+        }
+    });
+
+    next();
+});
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('upperCaseText', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageHeading: 'Home',
+        welcomeMessage: 'Welcome to my home page',
+    });
+});
+
+app.get('/aboutme', (req, res) => {
+    res.render('aboutme.hbs', {
+        pageHeading: 'About Me',
+    });
+});
+
+app.listen(3000, ()=> {
+    console.log('Web Server is listening on port 3000...');
+});
